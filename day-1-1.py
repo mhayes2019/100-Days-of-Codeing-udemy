0e75ccde1a7f9775c9c2caf3662d9077052a9308
mhayes2019 <54248688+mhayes2019@users.noreply.github.com>
2021-01-30 18:08:57 -0800

    Create day-1-1.py

---
+++ day-1-1.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")#if you want to use double quotes inside of another set of double quotes like this last example you will need to use double quotes and single quotes as shown above.
+
+
+
+
+
+
+
+
+
