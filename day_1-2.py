5df058fef664b1c4d68e99e075fa8f373d3999f5
mhayes2019 <54248688+mhayes2019@users.noreply.github.com>
2021-01-30 18:11:51 -0800

    Create day_1-2.py

---
+++ day_1-2.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+
+#first you can do your input fucntion to ask the user whatever question you want. 
+#I was not sure on how to print the length of the characters for whatever the user choose to type so I googled it. I googled "How to print the number of characters in a users input for python"
+#first I need to assign a variable to the output
+#then in order to print the length of whatever the user types I will need to use the len() function. 
+#I will need to use the print() function first then I will need to nest the len()function into the print function. When I type out the len() function inside the paraenthese of len I will put what I would like it to get the length of and in this case I want it to get the length of what the user input and then print it to the screen
+user = input("What is your name?")
+print(len(user))
+
+print(len(input("What is your nickname?")))
+
+
+
+
+
+
+
+
+
+
+
+
+
